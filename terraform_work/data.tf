b4c347df937c90c7519cad0cf4225679321eb393
lot8229 <lot8229@kakao.com>
2022-08-24 19:39:41 +0900

    VPC Networks Define

--- terraform_work/data.tf
+++ terraform_work/data.tf
@@ -6,3 +6,15 @@ data "aws_vpc" "this"{
 data "aws_subnet" "public1"{
     id = var.public_subnet1
 }
+data "aws_subnet" "public2"{
+    id = var.public_subnet2
+}
+data "aws_subnet" "public3"{
+    id = var.public_subnet3
+}
+data "aws_subnet" "public4"{
+    id = var.public_subnet4
+}
+data "aws_security_group" "this"{
+    id = var.sg_id
+}
