b82a41cfb6ddabc5e9753e5e843397314f736660
lot8229 <lot8229@kakao.com>
2022-08-26 08:28:30 +0900

    resources routetables

--- terraform_work/network.tf
+++ terraform_work/network.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc" "ecs_cluster_vpc" {
   tags = {
     "Name" = "ecs_cluster_vpc"
-    "Project" = "no-jam"
   }
   cidr_block = "10.0.0.0/16"
 }
@@ -9,29 +8,26 @@ resource "aws_subnet" "ecs_subnet_pub" {
   vpc_id = aws_vpc.ecs_cluster_vpc
   count = 2
   cidr_block = "10.30.${10 + count.index}.0/24"
-  availability_zone = data.aws_availbility_zones.available.names[0]
+  availability_zone = "${data.aws_availbility_zones.available.names[0]}"
   tags = {
     "Name" = "ecs_subnet_${1 + count.index}"
-    "Project" = "no-jam"
   }
 }
 
 resource "aws_subnet" "ecs_subnet_priv" {
-  vpc_id = aws_vpc.ecs_cluster_vpc
+  vpc_id = aws_vpc.ecs_cluster_vpc.id
   count = 2
   cidr_block = "10.30.${12 + count.index}.0/24"
-  availability_zone = data.aws_availbility_zones.available.names[2]
+  availability_zone = "${data.aws_availbility_zones.available.names[2]}"
   tags = {
     "Name" = "ecs_subnet_${2 + count.index}"
-    "Project" = "no-jam"
   }
 }
 
 resource "aws_internet_gateway" "ecs_igw" {
-  vpc_id = aws_vpc.ecs_cluster_vpc
+  vpc_id = aws_vpc.ecs_cluster_vpc.id
   tags = {
     "Name" = "ecs_igw"
-    "Project" = "no-jam"
   }
 }
 
@@ -43,19 +39,33 @@ resource "aws_eip" "nat_gw" {
 }
 
 resource "aws_nat_gateway" "ecs_ngw" {
-  allocation_id = aws_eip.nat_gw
+  allocation_id = aws_eip.nat_gw.id
   connectivity_type = "private"
-  subnet_id = aws_subnet.ecs_subnet_priv
+  subnet_id = aws_subnet.ecs_subnet_priv.id
 }
 
-resource "aws_route_table" "public_route" {
-  vpc_id = aws_vpc.ecs_cluster_vpc
+resource "aws_route_table" "priv_route" {
+  vpc_id = aws_vpc.ecs_cluster_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ecs_ngw.id
+  }
+  tags = {
+    "Name" = "priv_route_table"
+  }  
+}
+resource "aws_default_route_table" "default_route" {
+  default_route_table_id = "${aws_vpc.side_effect.default_route_table_id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.ecs_igw.id
   }
-  tags = {
-    "Name" = "ecs-route-table"
-    "Project" = "no-jam"
-  }  
+  tags {
+    Name = "public_route_table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnets_association" {
+  subnet_id = aws_subnet.ecs_subnet_pub.id
+  route_table_id = aws_default_route_table.default_route.id
 }
